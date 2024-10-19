1743681615fd43367dac81cbf1c64e96a5a58454
Ryan Reynald <ryanreynald67@gamil.com>
2024-10-19 17:30:46 -0300

    Breecrowd

---
+++ 1004.py
@@ -0,0 +1,5 @@
+p1 = int(input())
+p2 = int(input())
+
+PROD = p1 * p2
+print(f"PROD = {PROD}")
---
+++ 1008.py
@@ -0,0 +1,8 @@
+Numero = int(input())
+Hrs = int(input())
+valor = float(input())
+
+salario = valor * Hrs
+
+print(f"NUMBER = {Numero}")
+print(f"SALARY = U$ {salario:.2f}")
